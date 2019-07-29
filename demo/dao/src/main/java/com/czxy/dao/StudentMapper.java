9c9b54946ec131768fca85a3760cff9475d7f666
kuangwei <19976139162@qq.com>
2019-07-29 19:54:57 +0800

    Merge branch 'master' of F:\worksapce_idea2018\GitDemo with conflicts.

--- demo/dao/src/main/java/com/czxy/dao/StudentMapper.java
+++ demo/dao/src/main/java/com/czxy/dao/StudentMapper.java
@@ -1,17 +1,13 @@
 package com.czxy.dao;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
-
 import com.czxy.domain.Student;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface StudentMapper extends tk.mybatis.mapper.common.Mapper<Student> {
     List<Student> findByAll(Student student);
     int insertSelective(@Param("student")Student student);
-
-
-
-
 
 }
