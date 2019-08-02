f8e9a2b8d8edeb93231133e913d6764e6aae81bd
wangzhenyuxiansen <wzy980524>
2019-08-02 18:06:47 +0800

    修改了一下 添加课程  添加学院 删除学院的效果  和展示学院的效果1

--- demo/web/src/main/java/com/czxy/controller/UserController.java
+++ demo/web/src/main/java/com/czxy/controller/UserController.java
@@ -4,11 +4,13 @@ import com.czxy.domain.User;
 import com.czxy.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -27,4 +29,15 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+        @GetMapping("loginOut")
+        public ResponseEntity<Void> loginOut(HttpServletRequest request){
+            try {
+                request.getSession().removeAttribute("loginU");
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        }
 }
